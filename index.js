e4a5fb4d9881108d53719cb870e690bbf3500528
showaibbinnasir <showaibnishad@gmail.com>
2022-11-25 03:30:14 -0600

    added dotenv

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const port = process.env.PORT || 5000;
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+require('dotenv').config();
 
-
-// db-user : mobileDekho
-// db-pass : ryErWXqr6rSOG35P
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mobileDekho:ryErWXqr6rSOG35P@myfirstdb.w4kvmll.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@myfirstdb.w4kvmll.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
